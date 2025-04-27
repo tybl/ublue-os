0fc38a6de3e62a23d33130fdb8b8999d5080999c
tybl <t.brandon.lyons@gmail.com>
2025-04-26 20:04:38 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,6 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
+dnf5 update -y
 dnf5 install -y git git-lfs neovim tmux 
 
 # Use a COPR Example:
